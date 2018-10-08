9e035f01be08196967a41efe91fd4c76c5f835c1
Ryan Tablada <ryan.tablada@gmail.com>
2018-10-07 21:29:29 -0500

    Setup Decorators and Game List

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,29 @@
 module.exports = {
   root: true,
+  parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: "module"
   },
-  plugins: [
-    'ember'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended'
-  ],
+  plugins: ["ember"],
+  extends: ["eslint:recommended", "plugin:ember/recommended"],
   env: {
     browser: true
   },
-  rules: {
-  },
+  rules: {},
   overrides: [
     // node files
     {
       files: [
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/*/index.js'
+        ".template-lintrc.js",
+        "ember-cli-build.js",
+        "testem.js",
+        "blueprints/*/index.js",
+        "config/**/*.js",
+        "lib/*/index.js"
       ],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
         ecmaVersion: 2015
       },
       env: {
